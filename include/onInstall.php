d61bd1d1a768191ff830028208e69dfe97c5470b
mambax7 <mambax7@gmail.com>
2019-04-10 01:31:36 -0400

    function name conflict
    Signed-off-by: mambax7 <mambax7@gmail.com>

--- include/onInstall.php
+++ include/onInstall.php
@@ -2,14 +2,14 @@
 function xoops_module_install_tad_discuss(&$module)
 {
 
-    mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_discuss");
-    mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_discuss/thumbs");
+    tad_discuss_mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_discuss");
+    tad_discuss_mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_discuss/thumbs");
 
     return true;
 }
 
 //建立目錄
-function mk_dir($dir = "")
+function tad_discuss_mk_dir($dir = "")
 {
     //若無目錄名稱秀出警告訊息
     if (empty($dir)) {
--- include/onUninstall.php
+++ include/onUninstall.php
@@ -9,7 +9,7 @@ function xoops_module_uninstall_tad_discuss(&$module)
     return true;
 }
 
-function delete_directory($dirname)
+function tad_discuss_delete_directory($dirname)
 {
     if (is_dir($dirname)) {
         $dir_handle = opendir($dirname);
@@ -24,7 +24,7 @@ function delete_directory($dirname)
             if (!is_dir($dirname . "/" . $file)) {
                 unlink($dirname . "/" . $file);
             } else {
-                delete_directory($dirname . '/' . $file);
+                tad_discuss_delete_directory($dirname . '/' . $file);
             }
         }
     }
@@ -34,7 +34,7 @@ function delete_directory($dirname)
 }
 
 //拷貝目錄
-function full_copy($source = "", $target = "")
+function tad_discuss_full_copy($source = "", $target = "")
 {
     if (is_dir($source)) {
         @mkdir($target);
@@ -46,7 +46,7 @@ function full_copy($source = "", $target = "")
 
             $Entry = $source . '/' . $entry;
             if (is_dir($Entry)) {
-                full_copy($Entry, $target . '/' . $entry);
+                tad_discuss_full_copy($Entry, $target . '/' . $entry);
                 continue;
             }
             copy($Entry, $target . '/' . $entry);
